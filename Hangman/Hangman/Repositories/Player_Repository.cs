c972a30ac1853ef030c82c3b344c9dbf1a312c33
Iza Westman <izawestman@hotmail.com>
2020-09-09 10:18:07 +0200

    Lade till en ConnectionString till vår databas.

---
+++ Hangman/Hangman/Repositories/Player_Repository.cs
@@ -0,0 +1,26 @@
+﻿using Hangman.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Npgsql;
+
+
+namespace Hangman.Repositories
+{
+    class Player_Repository
+    {
+        //   private static string connectionMainString = ConfigurationManager.ConnectionStrings["dbMain"].ConnectionString;
+
+        #region CREATE
+
+        public static void CreatePlayer(Player player)
+        {
+            string stmt = "INSERT INTO player (name) values(@name)";
+
+         //        using (var conn = new NpsqlConnection())
+        }
+
+        #endregion
+    }
+}
