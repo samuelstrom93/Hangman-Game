9ec592c7d71caae86cd727bd65b98a6af4ddd3e4
a17emifu <kamonabe0@gmail.com>
2020-09-09 14:25:27 +0200

    Skapade ett gränssnittet Gameover_Page

---
+++ Hangman/Hangman/Views/Gameover_Page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hangman.Views
+{
+    /// <summary>
+    /// Gameover_Page.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Gameover_Page : Page
+    {
+        public Gameover_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Content = new GamePage();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Content = new Login();
+        }
+    }
+}
