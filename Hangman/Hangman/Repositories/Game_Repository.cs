8a60200a10c1590abb4cac183c99ee0d33383677
a17emifu <kamonabe0@gmail.com>
2020-09-09 11:14:13 +0200

    Skapade Game_Repository

---
+++ Hangman/Hangman/Repositories/Game_Repository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Hangman.Repositories
+{
+    class Game_Repository
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["dbMain"].ConnectionString;
+    }
+}
