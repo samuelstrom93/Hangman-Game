c895e24c93a0bc59605e7006fc38a2ef33671c32
a17emifu <kamonabe0@gmail.com>
2020-09-10 10:08:19 +0200

    ViewModels - BaseViewModel.cs, RelayCommand.cs, GamePageViewModel.cs

---
+++ Hangman/Hangman/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Hangman.ViewModels.Base
+{
+    class RelayCommand : ICommand
+    {
+        private Action action;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        /// <summary>
+        /// Metoden kan alltid exekveras
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+    }
+}
