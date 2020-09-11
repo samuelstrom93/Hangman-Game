2f0ec63af93ba2e8b05bed0edfcdd8b38d0280b2
Marcus Björkman <marcus.bjorkman@hotmail.com>
2020-09-11 12:29:53 +0200

    Splittat upp topplistorna i UserControls

---
+++ Hangman/Hangman/Views/Highscore/HighScore_Page.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace Hangman.Views
+{
+    /// <summary>
+    /// HighScore_Page.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HighScore_Page : Page
+    {
+        public HighScore_Page()
+        {
+            InitializeComponent();
+
+            TopPlayerHighScores.Content = new TopGamesUC(28);
+            TopHighScores.Content = new TopGamesUC();
+            TopPlayers.Content = new TopPlayersUC();
+        }
+    }
+}
---
+++ Hangman/Hangman/Views/Highscore/TopGamesUC.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Hangman.Repositories;
+using Hangman.ViewModels;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace Hangman.Views
+{
+    /// <summary>
+    /// Interaction logic for TopGamesUC.xaml
+    /// </summary>
+    public partial class TopGamesUC : UserControl
+    {
+        public TopGamesUC(int? playerId = null)
+        {
+            InitializeComponent();
+
+            HighscoresViewModel vm = new HighscoresViewModel()
+            {
+                TopHighscores = HighscoreRepository.GetTopGames(playerId).ToList()
+            };
+
+            DataContext = vm;
+        }
+    }
+}
