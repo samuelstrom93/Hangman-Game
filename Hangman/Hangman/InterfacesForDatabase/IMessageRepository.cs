2228995adad865affbb15a6f2e65d6a62acfb664
Emiko Johansson <emjo1804@student.miun.se>
2020-09-24 11:51:39 +0200

    Interface med public metoder på MessageRepository

--- Hangman/Hangman/InterfacesForDatabase/IMessageRepository.cs
+++ Hangman/Hangman/InterfacesForDatabase/IMessageRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hangman.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,9 @@ namespace Hangman.Database
 {
     interface IMessageRepository
     {
+        IEnumerable<Message> GetMessages(int? recieverId, int? senderId);
+        Message GetSingleMessage(int id);
+        bool TryAddMessage(Message message, out Message added);
+        bool TryAddMessage(string topic, string content, int senderId, int recieverId, out Message added);
     }
 }
