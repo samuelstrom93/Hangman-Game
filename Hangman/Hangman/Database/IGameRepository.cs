29bd4cef21e07a15285130e0e3c9bd9c6c9ef4fe
Emiko Johansson <emjo1804@student.miun.se>
2020-09-24 10:38:20 +0200

    Mappens namn ändrats till "Database" Interfaces skapats (innehåll är tömma förutom IGameRepository)

---
+++ Hangman/Hangman/Database/IGameRepository.cs
@@ -0,0 +1,16 @@
+﻿using Hangman.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hangman.Moduls.InterfacesForDatabase
+{
+    interface IGameRepository
+    {
+        int AddGame(Game game);
+        Game GetGameFromID(int gameID);
+        Game GetGameFromPlayerID(int playerID);
+        Game GetGameFromWordID(int wordID);
+        void DeleteGame(int gameID);
+    }
+}
