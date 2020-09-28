4c859d320ee0a478eee57624a464b4f198a9d003
Iza Westman <izawestman@hotmail.com>
2020-09-28 09:55:01 +0200

    Bytt plats på Logga in i StartUpPage

--- Hangman/Hangman/ViewModels/StartUpPageViewModel.cs
+++ Hangman/Hangman/ViewModels/StartUpPageViewModel.cs
@@ -10,10 +10,18 @@ namespace Hangman.ViewModels
 {
     class StartUpPageViewModel : BaseViewModel
     {
+        public ICommand LogOutCommand { get; set; } //Binding i StartUpPage.xaml
+
         public StartUpPageViewModel()
         {
-          
+            LogOutCommand = new RelayCommand(LogOut);
 
+        }
+
+        private void LogOut()
+        {
+            SetActivePlayer(null);
+            GoToPage(ApplicationPage.Login);
         }
     }
 }
