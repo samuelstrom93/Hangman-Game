b38671f4308093a79d7cacc1a0075cc1bf5bf823
Samuel Ström <samuelstrom93@gmail.com>
2020-09-29 14:03:24 +0200

    Slimmat koden - parametrar som indata för databasmetoderna

--- Hangman/Hangman/InterfacesForDatabase/IHighscoreRepository.cs
+++ Hangman/Hangman/InterfacesForDatabase/IHighscoreRepository.cs
@@ -8,7 +8,6 @@ namespace Hangman.Database
 {
     public interface IHighscoreRepository
     {
-        IEnumerable<HighscoreGame> GetTopGames(int? playerID, int numHighscore);
         ObservableCollection<HighscoreGame> GetLeaderboard(int? playerID = null, int numHighscores = 20);
         IDictionary<string, long> GetTopDiligentPlayers(int numPlayers);
         int GetRankOnHighScore(int gameID);
--- Hangman/Hangman/Repositories/HighscoreRepository.cs
+++ Hangman/Hangman/Repositories/HighscoreRepository.cs
@@ -19,54 +19,6 @@ namespace Hangman.Repositories
         /// <param name="playerId">SpelarId om man vill hämta topplista för en specifik spelare</param>
         /// <param name="numHighscores">Hur lång toppplista man vill ha</param>
         /// <returns>Lista med toppspel</returns>
-        public IEnumerable<HighscoreGame> GetTopGames(int? playerId = null, int numHighscores = 10)
-        {
-            var result = new List<HighscoreGame>();
-            var queryString = "select number_of_tries, number_of_incorrect_tries, player.name as player_name, word.name as word_name, (select end_time - start_time as game_time) from game"
-                + "\ninner join player on player.id = player_id"
-                + "\ninner join word on word.id = word_id"
-                + "\nwhere is_won is true"
-                + $"{(playerId.HasValue ? $" and player_id=@playerid" : string.Empty)}"
-                + "\norder by number_of_incorrect_tries, game_time"
-                + $"\nlimit {numHighscores}";
-
-            using (var conn = new NpgsqlConnection(_connectionString))
-            {
-                conn.Open();
-                using (var command = new NpgsqlCommand(queryString, conn))
-                {
-                    if (playerId.HasValue)
-                    {
-                        command.Parameters.AddWithValue("playerid", playerId.Value);
-                    }
-                    using (var reader = command.ExecuteReader())
-                    {
-                        if (!reader.HasRows)
-                        {
-                            return result;
-                        }
-
-                        while (reader.Read())
-                        {
-                            var row = new HighscoreGame
-                            {
-                                PlayerName = (string)reader["player_name"],
-                                Word = (string)reader["word_name"],
-                                NumberOfTries = (int)reader["number_of_tries"],
-                                NumberOfIncorrectTries = (int)reader["number_of_incorrect_tries"],
-                                GameTime = (TimeSpan)reader["game_time"]
-                            };
-
-                            result.Add(row);
-                        }
-                    }
-                }
-            }
-
-            return result;
-        }
-
-
         public ObservableCollection<HighscoreGame> GetLeaderboard(int? playerId = null, int numHighscores = 20)
         {
             var result = new ObservableCollection<HighscoreGame>();
@@ -105,7 +57,6 @@ namespace Hangman.Repositories
                             {
                                 PlayerName = (string)reader["player_name"],
                                 Word = (string)reader["word_name"],
-                                //NumberOfTries = (int)reader["number_of_tries"],
                                 NumberOfIncorrectTries = (int)reader["number_of_incorrect_tries"],
                                 GameTime = (TimeSpan)reader["game_time"],
                                 RankOnLeaderboard = (int)reader["rank"]
@@ -133,13 +84,15 @@ namespace Hangman.Repositories
                 + "\nwhere is_won is true"
                 + "\ngroup by player_name"
                 + "\norder by count desc"
-                + $"\nlimit {numPlayers}";
+                + $"\nlimit @numplayers";
 
             using (var conn = new NpgsqlConnection(_connectionString))
             {
                 conn.Open();
                 using (var command = new NpgsqlCommand(queryString, conn))
                 {
+                    command.Parameters.AddWithValue("numplayers", numPlayers);
+
                     using (var reader = command.ExecuteReader())
                     {
                         if (!reader.HasRows)
@@ -161,11 +114,12 @@ namespace Hangman.Repositories
 
         public int GetRankOnHighScore(int gameId)
         {
-            string stmt = $"WITH leaderboard as (SELECT*, RANK () OVER(ORDER BY number_of_incorrect_tries, game_time) FROM (SELECT id, number_of_incorrect_tries, (SELECT end_time - start_time AS game_time) FROM game WHERE is_won IS true) AS rows) SELECT CAST(rank as integer), number_of_incorrect_tries, game_time FROM leaderboard WHERE id = {gameId}";
+            string stmt = $"WITH leaderboard as (SELECT*, RANK () OVER(ORDER BY number_of_incorrect_tries, game_time) FROM (SELECT id, number_of_incorrect_tries, (SELECT end_time - start_time AS game_time) FROM game WHERE is_won IS true) AS rows) SELECT CAST(rank as integer), number_of_incorrect_tries, game_time FROM leaderboard WHERE id = @gameId";
 
             using (var conn = new NpgsqlConnection(_connectionString))
             using (var command = new NpgsqlCommand(stmt, conn))
             {
+                command.Parameters.AddWithValue("gameId", gameId);
                 conn.Open();
                 return (int)command.ExecuteScalar();
             }
