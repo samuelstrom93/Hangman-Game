c8b871803724a4faadd5c4601aa9fe10a2f3b064
Samuel Ström <samuelstrom93@gmail.com>
2020-09-30 15:21:18 +0200

    Tagit bort överflödiga kommentarer

--- Hangman/Hangman/Helper/NavigateToPageHelper.cs
+++ Hangman/Hangman/Helper/NavigateToPageHelper.cs
@@ -11,15 +11,6 @@ namespace Hangman.Helper
 {
     public static class NavigateToPageHelper
     {
-        /// <summary>
-        /// Returnerar en Page utifrån vald sida
-        /// </summary>
-        /// <param name="page">Indata av  typen <see cref="ApplicationPage"/></param>
-        /// <param name="model">Specifik vymodell som ska laddas, default = null <</param>
-        /// <returns></returns>
-        /// 
-        
-            //Ändrat Application -> Page - rätt eller fel?
         public static Page GetPage(ApplicationPage page, BaseViewModel model = null)
         {
             return page switch
@@ -35,7 +26,6 @@ namespace Hangman.Helper
                 ApplicationPage.HighscorePage => new HighScore_Page(model),
                 ApplicationPage.StartUpPage => new StartUpPage(model),
                 _ => null,
-
             };
         }
     }
